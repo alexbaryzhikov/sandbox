2bd2625a7a8890b0874b44bcccf4124ffb8457a0
Alex Baryzhikov <aleksiarts@gmail.com>
2022-12-28 10:22:21 +0100

    Update primes_iterator.cpp

--- cpp/primes_iterator.cpp
+++ cpp/primes_iterator.cpp
@@ -4,35 +4,32 @@
 
 class PrimeIterator {
    private:
-    std::unordered_map<int, std::vector<int>> D;
-    int q;
+    std::unordered_map<long, std::vector<long>> D = {{4, {2}}};
+    int q = 2;
 
    public:
-    PrimeIterator() : D{{4, {2}}}, q(2) {}
-
     int operator*() { return q; }
 
     PrimeIterator& operator++() {
-        while (true) {
-            if (D.find(++q) == D.end()) {
-                D[q * q] = {q};
-                break;
-            } else {
-                for (int p : D[q]) {
-                    D[p + q].push_back(p);
-                }
-                D.erase(q);
+        while (D.find(++q) != D.end()) {
+            for (int p : D[q]) {
+                D[p + q].push_back(p);
             }
+            D.erase(q);
         }
+        D[q * q] = {q};
         return *this;
     }
 };
 
 int main() {
     PrimeIterator it;
-    for (int i = 0; i < 100; ++i) {
-        std::cout << *it << ' ';
+    int prime_index;
+    std::cout << "Enter prime number index: ";
+    std::cin >> prime_index;
+    for (int i = 1; i < prime_index; ++i) {
         ++it;
     }
+    std::cout << "Prime number #" << prime_index << " is " << *it << std::endl;
     return 0;
 }
