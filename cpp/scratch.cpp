8a8e37c80b1474391ce1084426b076df69a6f4f9
Alex Baryzhikov <aleksiarts@gmail.com>
2025-03-01 17:42:15 +0100

    Update scratch.cpp

--- cpp/scratch.cpp
+++ cpp/scratch.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <ranges>
 
 int main() {
-  float a = 1.5;
-  int b = 2;
-  std::cout << b - a << '\n';
+    std::vector<int> v = {1, 2, 3, 4, 5};
+    
+    auto evens = v | std::views::filter([](int x){ return x % 2 == 0; });
+
+    for (int x : evens) {
+        std::cout << x << ' ';
+    }
+
+    std::cout << '\n';
 }
